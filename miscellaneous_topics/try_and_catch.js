c51d83ff83bf72eacacc00641c784fe9cfdff71c
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-05-15 02:02:21 +0530

    Add files via upload

---
+++ miscellaneous_topics/try_and_catch.js
@@ -0,0 +1,15 @@
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+
+try{
+    console.log(a);
+}
+catch(err){
+    console.log("a not defined");
+    console.log(err);
+}
+
+console.log("Hello2");
+console.log("Hello2");
+console.log("Hello2");
