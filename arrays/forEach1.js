1f70e7e0c51bb990950e14f6989c9f5763c6104e
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-05-26 16:19:46 +0530

    Add files via upload

---
+++ arrays/forEach1.js
@@ -0,0 +1,17 @@
+let arr = [
+{
+    Name: "Niomi",
+    Marks: 85.85
+},
+{
+    Name: "Vinay",
+    Marks: 90.5
+},
+{
+    Name: "Sarthak",
+    Marks: 89
+}]
+
+arr.forEach((student)=>{
+    console.log(student.Name);
+});
