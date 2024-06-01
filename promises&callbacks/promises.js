382e1dc79d723f91dfa1b1193b492b0037c9bd65
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-06-01 13:42:12 +0530

    Update promises.js

--- promises&callbacks/promises.js
+++ promises&callbacks/promises.js
@@ -23,3 +23,16 @@
 // }, ()=>{
 //     console.log("Weak connection");
 // })
+
+function SavetoDb(data){
+    return new Promise((resolve, reject)=>{
+        let internetSpeed = Math.floor(Math.random() * 10)+1;
+
+        if (internetSpeed > 4){
+            resolve("Data saved successfully");
+        }
+        else{
+            reject("Weak connection");
+        }
+    });
+}
