4b15534ef49f18ad41090c4b0cab339a51d0ecfb
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-06-01 13:22:03 +0530

    Add files via upload

---
+++ promises&callbacks/promises.js
@@ -0,0 +1,25 @@
+function SavetoDb(data, success, failure){
+    let internetSpeed = Math.floor(Math.random() * 10)+1;
+
+    if (internetSpeed > 4){
+        success();
+    }
+    else{
+        console.log("Weak connection!");
+    }
+}
+SavetoDb("Niomi", ()=>{
+    console.log("Succesfully data stored!");
+    SavetoDb("Soni", ()=>{
+        console.log("SucceLsfully data stored!");
+        SavetoDb("Hello World", ()=>{
+            console.log("SucceLsfully data stored!");
+        }, ()=>{
+            console.log("Weak connection");
+        })
+    }, ()=>{
+        console.log("Weak connection");
+    })
+}, ()=>{
+    console.log("Weak connection");
+})
