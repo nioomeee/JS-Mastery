badfc1c5dcd62337fcf7528bee2c8f003c8db75e
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-06-01 14:27:33 +0530

    Update promise-chaining.js

--- promises&callbacks/promise-chaining.js
+++ promises&callbacks/promise-chaining.js
@@ -14,10 +14,10 @@ function SavetoDb(data){
 SavetoDb("Hello World")
     .then(()=>{
         console.log("Data saved successfully");
-        SavetoDb("Niomi")
-        .then(()=>{
-            console.log("Data saved successfully");
-        })
+        return SavetoDb("Niomi");
+    })
+    .then(()=>{
+        console.og("Data saved successfully");
     })
    .catch(()=>{
                 console.log("Weak connection");
