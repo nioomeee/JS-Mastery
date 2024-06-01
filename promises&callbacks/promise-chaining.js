e8a3220c2e84f4ff9c8b202f08f4c4fdbd6c6b83
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-06-01 14:22:14 +0530

    Add files via upload

---
+++ promises&callbacks/promise-chaining.js
@@ -0,0 +1,24 @@
+function SavetoDb(data){
+    return new Promise((resolve, reject)=>{
+        let internetSpeed = Math.floor(Math.random() * 10)+1;
+
+        if (internetSpeed > 4){
+            resolve("Success");
+        }
+        else{
+            reject("Failure");
+        }
+    });
+}
+
+SavetoDb("Hello World")
+    .then(()=>{
+        console.log("Data saved successfully");
+        SavetoDb("Niomi")
+        .then(()=>{
+            console.log("Data saved successfully");
+        })
+    })
+   .catch(()=>{
+                console.log("Weak connection");
+            });
