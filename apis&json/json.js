812392a0d1e480d3d29e1816eca0dfca68f18200
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-06-01 23:42:00 +0530

    Add files via upload

---
+++ apis&json/json.js
@@ -0,0 +1,13 @@
+let jsonRes = '{"fact":"A cat usually has about 12 whiskers on each side of its face.","length":61}';
+
+console.log(jsonRes);
+
+let validRes = JSON.parse(jsonRes);
+
+console.log(validRes);
+console.log(validRes.fact);
+
+
+
+
+
