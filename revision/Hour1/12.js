3e709f3de93bb3acecc87576362f3e7fa44380ec
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-09-18 17:09:26 +0530

    Add files via upload

---
+++ revision/Hour1/12.js
@@ -0,0 +1,10 @@
+// Create a simple module with two functions (e.g., add() and subtract()), export them, and import them in a separate file to use.
+
+export function add(a, b)
+{
+    return a+b;
+}
+export function sub(a, b)
+{
+    return a-b;
+}
