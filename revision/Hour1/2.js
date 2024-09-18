d6df35fb5a2ceee23490524d77a431c24d16a058
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-09-18 17:02:49 +0530

    Add files via upload

---
+++ revision/Hour1/2.js
@@ -0,0 +1,14 @@
+// Declare a const array. Try adding, removing, and modifying elements of the array. Can the array be mutated? Can it be reassigned?
+const arr = [3, 4, 5, 7];
+
+arr.push(1);
+
+console.log(arr);
+
+arr.pop();
+
+console.log(arr);
+
+arr[2] = 7;
+
+console.log(arr);
