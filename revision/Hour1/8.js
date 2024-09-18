71a1cddc9e2e61e5989ac632204de90e7958b9ec
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-09-18 17:07:38 +0530

    Add files via upload

---
+++ revision/Hour1/8.js
@@ -0,0 +1,10 @@
+// Given an array [10, 20, 30, 40], destructure it to assign values to individual variables.
+
+let arr = [10, 20, 30, 40];
+let [a, b, c, d] = arr;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
