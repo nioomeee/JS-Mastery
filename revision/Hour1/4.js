daa40ab9757986099673e9c7095cb69566d4fa8a
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-09-18 17:03:43 +0530

    Add files via upload

---
+++ revision/Hour1/4.js
@@ -0,0 +1,8 @@
+// Create an arrow function that takes no arguments and returns the string "Hello, World!".
+
+let func=()=>{
+    return "HWllo world!";
+}
+let print=func();
+
+console.log(`${print}`);
