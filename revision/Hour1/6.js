a13b9918c4c1c7983f66acb5cff74af0b0a9cd04
Niomi Soni <146643629+nioomeee@users.noreply.github.com>
2024-09-18 17:05:23 +0530

    Add files via upload

---
+++ revision/Hour1/6.js
@@ -0,0 +1,14 @@
+// Write a function that takes a string and embeds it in a sentence using template literals. Example:
+// function greet(name) {
+//   return `Hello, ${name}!`;
+// }
+
+let str="Hello World!";
+
+let fun=()=>{
+    return `${str} I am programmer`;
+}
+
+let print = fun();
+
+console.log(`${print}`);
